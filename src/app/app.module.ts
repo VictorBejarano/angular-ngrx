0f79d06baae1808ab4fa98bfc77824969d7ecc3e
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-06-12 22:27:23 -0500

    Implement State DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,27 @@ import { NgModule } from '@angular/core';
 
 // NGRX
 import { StoreModule } from '@ngrx/store';
+import { contadorReducer } from './contador/contador.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
-import { contadorReducer } from './contador/contador.reducer';
-
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HijoComponent,
-    NietoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot({ contador: contadorReducer})
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HijoComponent, NietoComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        StoreModule.forRoot({ contador: contadorReducer }),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: environment.production,
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
